c5d04365147a615c72ee9f85f98ec6f97dc52b3c
kalabalasalabu <152157222+kalabalasalabu@users.noreply.github.com>
2024-05-20 15:13:03 +0200

    Add files via upload

---
+++ demo/src/main/java/au/com/marciomarinho/controller/model/ProductID.java
@@ -0,0 +1,7 @@
+package au.com.marciomarinho.controller.model;
+
+public class ProductID {
+    private final String id;
+    public ProductID(final String id){this.id=id;}
+    public String getId(){return this.id;}
+}
